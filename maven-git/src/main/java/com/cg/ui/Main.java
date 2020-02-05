9a8dd04c36cc6255c10d1ab6958f839f242a4fa1
shivani1805 <shivani.sharma702@gmail.com>
2020-02-05 16:04:06 +0530

    commit 3

--- maven-git/src/main/java/com/cg/ui/Main.java
+++ maven-git/src/main/java/com/cg/ui/Main.java
@@ -3,7 +3,7 @@ package com.cg.ui;
 public class Main {
 	public static void main(String[] args)
 	{
-		System.out.println("This is new project");
+		System.out.println("This is new project by shivani");
 		
 	}
 	
