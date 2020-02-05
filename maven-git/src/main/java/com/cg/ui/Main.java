daf73a6e8cde716179bdffb35157b01eca2cac97
shivani1805 <shivani.sharma702@gmail.com>
2020-02-05 16:03:09 +0530

    commit 2

--- maven-git/src/main/java/com/cg/ui/Main.java
+++ maven-git/src/main/java/com/cg/ui/Main.java
@@ -3,7 +3,7 @@ package com.cg.ui;
 public class Main {
 	public static void main(String[] args)
 	{
-		System.out.println("This is shivani");
+		System.out.println("This is new project");
 		
 	}
 	
